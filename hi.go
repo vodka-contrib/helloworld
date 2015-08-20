b4fd173546ba98b1f04b2db5be324ea0834e0f0e
insion <insion@live.com>
2015-08-20 11:27:14 +0800

    Create hi.go

---
+++ hi.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/insionng/vodka"
+	m "github.com/insionng/vodka/middleware"
+)
+
+func main() {
+
+	v := vodka.New()
+	v.Use(m.Logger())
+	v.Use(m.Recover())
+	v.Use(m.Gzip())
+
+// Handler
+func HiHandler(self *vodka.Context) error {
+    return self.String(http.StatusOK, "Hi, World!")
+}
+
+ v.Get("/", HiHandler)
+
+// Start server
+ v.Run(":9000")
+}
