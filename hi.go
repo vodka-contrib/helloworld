a9d1ee21bda8b9269552c38c00ad0ac94fe65139
insion <insion@live.com>
2015-09-15 13:14:55 +0800

    Update hi.go

--- hi.go
+++ hi.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/insionng/vodka"
 	m "github.com/insionng/vodka/middleware"
 )
 
-func main() {
-
-	v := vodka.New()
-	v.Use(m.Logger())
-	v.Use(m.Recover())
-	v.Use(m.Gzip())
-
 // Handler
 func HiHandler(self *vodka.Context) error {
     return self.String(http.StatusOK, "Hi, World!")
 }
 
+func main() {
+
+ v := vodka.New()
+ v.Use(m.Logger())
+ v.Use(m.Recover())
+ v.Use(m.Gzip())
+ 
  v.Any("/", HiHandler)
 
-// Start server
+ // Start server
  v.Run(9000)
  //v.Run(":9000")
  //v.Run("localhost")
+ 
 }
