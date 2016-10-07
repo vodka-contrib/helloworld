4e7eba8ee0c9d5dd38fd23224facf8b2f930c7c5
insion <insion@live.com>
2016-10-07 18:35:42 +0800

    just for Vodka V2.0.0+

--- hi.go
+++ hi.go
@@ -4,26 +4,20 @@ import (
 	"net/http"
 
 	"github.com/insionng/vodka"
+	"github.com/insionng/vodka/engine/fasthttp"
 	m "github.com/insionng/vodka/middleware"
 )
 
-// Handler
-func HiHandler(self *vodka.Context) error {
-    return self.String(http.StatusOK, "Hi, World!")
+func main() {
+	v := vodka.New()
+	v.Use(m.Logger())
+	v.Use(m.Recover())
+	v.Use(m.Gzip())
+
+	v.GET("/", HiHandler)
+	v.Run(fasthttp.New(":1987"))
 }
 
-func main() {
-
- v := vodka.New()
- v.Use(m.Logger())
- v.Use(m.Recover())
- v.Use(m.Gzip())
- 
- v.Any("/", HiHandler)
-
- // Start server
- v.Run(9000)
- //v.Run(":9000")
- //v.Run("localhost")
- 
+func HiHandler(self vodka.Context) error {
+	return self.String(http.StatusOK, "Hi, World!")
 }
